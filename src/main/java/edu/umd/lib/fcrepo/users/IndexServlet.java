d087cee552306fa9ea0a71310996e3df0b58f79d
Jeremy Matthew Gottwig <jgottwig@umd.edu>
2016-07-07 08:25:02 -0400

    LIBFCREPO-55. Adding referrer redirect

--- src/main/java/edu/umd/lib/fcrepo/users/IndexServlet.java
+++ src/main/java/edu/umd/lib/fcrepo/users/IndexServlet.java
@@ -1,6 +1,7 @@
 package edu.umd.lib.fcrepo.users;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,12 +9,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/")
 public class IndexServlet extends HttpServlet {
 
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
-    dispatcher.forward(request, response);
+
+    HttpSession session = request.getSession();
+    String storedReferer = (String) session.getAttribute("referer");
+
+    /**
+     * Verify our session referrer and validate the URL Either redirect or
+     * forward
+     */
+    Pattern p = Pattern.compile("^(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$");
+    if (storedReferer != null &&
+        p.matcher(storedReferer).matches()) {
+      // In this case, we redirect
+      session.setAttribute("referer", null); // Prevents repeat behavior
+      response.sendRedirect(storedReferer);
+    } else {
+      // Otherwise, display the profile page
+      dispatcher.forward(request, response);
+    }
   }
 }
--- src/main/java/edu/umd/lib/fcrepo/users/LoginServlet.java
+++ src/main/java/edu/umd/lib/fcrepo/users/LoginServlet.java
@@ -8,17 +8,42 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     RequestDispatcher dispatcher;
+    String redirect = request.getParameter("redirect");
+    String referer = request.getHeader("referer");
+
+    HttpSession session = request.getSession();
+    String storedReferer = (String) session.getAttribute("referer");
+
     if (request.getRemoteUser() == null) {
+
+      /**
+       * Check for referrer and redirect. Store referrer into session.
+       *
+       * @note URL validation included elsewhere
+       * @see IndexServlet.java
+       */
+      if (redirect != null &&
+          redirect.equals("true") &&
+          !referer.isEmpty() &&
+          (storedReferer == null || storedReferer.isEmpty())) {
+        session.setAttribute("referer", referer);
+      }
       // display the login form
       dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
     } else {
-      // already logged in, display the profile page
+      /**
+       * Or forward to index controller
+       *
+       * @note Redirection happens on IndexServlet.java
+       */
       dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
     }
     dispatcher.forward(request, response);
